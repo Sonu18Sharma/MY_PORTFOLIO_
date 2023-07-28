48c3dc225d2a65bb3b37e74c42ee54b823b011c5
sonu sharma <121657901+Sonu18Sharma@users.noreply.github.com>
2023-07-28 11:45:53 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,9 @@
+// const hamburgerMenu = document.querySelector('.hamburger-menu');
+// const menu = document.querySelector('.menu');
+
+// hamburgerMenu.addEventListener('click', () => {
+//   hamburgerMenu.classList.toggle('active');
+//   menu.classList.toggle('open');
+// });
+
+  
